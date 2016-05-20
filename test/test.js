fc624145d11fde848029840c74891c4b0f44ba90
thomasweld <thomas.weld@gmail.com>
2016-05-19 20:23:54 -0400

    tests update

--- test/test.js
+++ test/test.js
@@ -14,8 +14,18 @@ describe('Something We Want To Test', function () {
   describe('Testing the Creation of Something', function () {
 
     it('should exist after we create it', function () {
-      let x = new Object;
-      expect(x).to.be.an.instanceof(Object);
+      let x = new AppController;
+      expect(x).to.be.an.instanceof(AppController);
+    });
+
+    it('should exist after we create it', function () {
+      let x = new ContactList;
+      expect(x).to.be.an.instanceof(ContactList);
+    });
+
+    it('should exist after we create it', function () {
+      let x = new SingleContact;
+      expect(x).to.be.an.instanceof(SingleContact);
     });
 
   });
